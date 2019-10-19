4ed4f6b70e6be8f1fc537e5914b313489d2e6222
run-after <run.after@protonmail.com>
2019-10-18 19:20:55 -0500

    corrected algorithm to calculate profit instead of just finding lowest price to buy at

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,33 +1,21 @@
 def stock_picker(array)
 
-    largest = 0
-    smallest = 1000
-    largest_index = 1
-    smallest_index = 0
+    hash = Hash.new
 
-array.each_with_index { |small, small_index| 
+array.each_with_index { |buy, buy_index| 
     
-    array.each_with_index { |large, large_index| 
+    array.each_with_index { |sell, sell_index| 
         
-    if small < smallest && small_index < largest_index
-     
-            smallest = small
-            smallest_index = small_index
-        #puts 'smallest = ' + smallest.to_s
-        
-    end
-    if large > largest && large_index > smallest_index 
-        
-            largest = large
-            largest_index = large_index
-        #puts "largest = " + largest.to_s
-    end
-        
-
+        if sell_index > buy_index
+            total = sell - buy
+            
+            hash[[buy_index, sell_index]] = total
+    
+        end
         }
     }
 
-     [smallest_index, largest_index]
+     hash.key(hash.values.max)
 
 end
  p stock_picker([17,3,6,9,15,8,6,1,10])
@@ -35,3 +23,4 @@ end
  p stock_picker([8, 5, 3, 6 ,8, 56, 43, 76, 54, 9])
  p stock_picker([6, 2, 7, 3, 1, 7, 3, 8, 4, 9])
  p stock_picker([99, 88, 77, 66, 55, 44, 33, 22, 11, 99])
+ p stock_picker([80, 99, 3,4,7,10])
